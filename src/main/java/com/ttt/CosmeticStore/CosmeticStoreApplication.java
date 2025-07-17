e4a46aa2f1dee2b2aba99b00a51781033538a1dc
Thuong Tran <t.t.thuong2004@gmail.com>
2025-07-17 08:24:45 +0700

    signup signin

---
+++ src/main/java/com/ttt/CosmeticStore/CosmeticStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ttt.CosmeticStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmeticStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttt/CosmeticStore/CosmeticStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttt.CosmeticStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
